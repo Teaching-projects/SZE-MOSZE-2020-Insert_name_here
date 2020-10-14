6abadd0e90038891575799b2d117b10ab0cd8e3c
mogyorosimartin <43951510+mogyorosimartin@users.noreply.github.com>
2020-10-14 17:25:37 +0000

    Update gh-pages to output generated at 6f47057

--- search/all_3.js
+++ search/all_3.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['game',['Game',['../classGame.html',1,'Game'],['../classCharacter.html#aa2fab026580d6f14280c2ffb8063a314',1,'Character::Game()']]],
+  ['game',['Game',['../classGame.html',1,'']]],
   ['getattackspeed',['getAttackspeed',['../classCharacter.html#a0405fa66f7ac4e1a6a7faf1bffe2c5c7',1,'Character']]],
   ['getdamage',['getDamage',['../classCharacter.html#a4b5771117324e7bcf7afd005b2aa5943',1,'Character']]],
   ['gethealth',['getHealth',['../classCharacter.html#a39eeebcbdbc13cd4d1d274da5d051f85',1,'Character']]],
--- search/searchdata.js
+++ search/searchdata.js
@@ -3,8 +3,7 @@ var indexSectionsWithContent =
   0: "acfgps",
   1: "cg",
   2: "acfgp",
-  3: "g",
-  4: "s"
+  3: "s"
 };
 
 var indexSectionNames =
@@ -12,8 +11,7 @@ var indexSectionNames =
   0: "all",
   1: "classes",
   2: "functions",
-  3: "related",
-  4: "pages"
+  3: "pages"
 };
 
 var indexSectionLabels =
@@ -21,7 +19,6 @@ var indexSectionLabels =
   0: "All",
   1: "Classes",
   2: "Functions",
-  3: "Friends",
-  4: "Pages"
+  3: "Pages"
 };
 
