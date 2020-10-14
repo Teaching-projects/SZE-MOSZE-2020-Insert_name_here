3d2ea73352373366da9cce489cd6f314f314ea98
bartaliszoltan <zoltanbartalis98@gmail.com>
2020-10-14 20:18:40 +0200

    add +3 test with levelup

--- test_01.sh
+++ test_01.sh
@@ -9,4 +9,6 @@ file='output.txt'
 ./a.out units/unit2.json units/unit3.json >> $file
 ./a.out units/unit3.json units/unit1.json >> $file
 ./a.out units/unit3.json units/unit2.json >> $file
-
+./a.out units/player.json units/unit3.json >> $file
+./a.out units/player.json units/unit2.json >> $file
+./a.out units/player.json units/unit1.json >> $file
