0779c20fc2077dfd483e39e8f25dc52bfe5d37d7
bartaliszoltan <zoltanbartalis98@gmail.com>
2020-10-15 02:12:07 +0200

    fix reading Player from player.json for testing

--- main.cpp
+++ main.cpp
@@ -14,13 +14,13 @@ int main(int argc, char *argv[]) {
 		std::string fname2 = argv[2];
 
 	Game G;
-	if (fname1 == "player.json") {
+	if (fname1 == "units/player.json") {
 		Player p1(Player::parseUnit(fname1));
 		Character c1(Character::parseUnit(fname2));
 		G.attack(p1, c1);
 		G.printWhoWins(p1, c1);
 	}
-	else if (fname2 == "player.json") {
+	else if (fname2 == "units/player.json") {
 		Character c1(Character::parseUnit(fname1));
 		Player p1(Player::parseUnit(fname2));
 		G.attack(c1, p1);
