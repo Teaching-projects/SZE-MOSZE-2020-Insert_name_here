dd0cf6dfa11737023ace023846377bc50b48c215
Mogyorósi Martin <mogyorosi.martin6@gmail.com>
2020-10-21 19:40:02 +0200

    Separate function into two

--- jsonparser.cpp
+++ jsonparser.cpp
@@ -9,23 +9,27 @@ std::map <std::string, std::string> Parser::jsonParser(std::istream& file) {
 }
 
 
+std::string Parser::jsonParserFILE(const std::string& filename){
+	std::string line,alldata;
+	std::ifstream file;
+	file.open(filename);
+	if (file.fail()) throw "The " + filename + " file does not exist or is not readable.";
 
+	while (getline(file, line)) alldata += line;
+	file.close();
+	return alldata;
+}
 
 
 std::map <std::string, std::string> Parser::jsonParser(const std::string& fname) {
 
-	std::string line, alldata, type = "";
+	std::string alldata, type = "";
 	std::map<std::string, std::string> data;
 
 
 	if (fname.find('\"') + 1) alldata = fname;
 	else {
-		std::ifstream file;
-		file.open(fname);
-		if (file.fail()) throw "The " + fname + " file does not exist or is not readable.";
-
-		while (getline(file, line)) alldata += line;
-		file.close();
+		alldata=jsonParserFILE(fname);
 	}
 
 
@@ -58,7 +62,8 @@ std::map <std::string, std::string> Parser::jsonParser(const std::string& fname)
 			type = "";
 		}
 		
-	}	
+	}
+	
 	if(!data.count("name") or !data.count("hp") or !data.count("dmg")) throw std::string("Missing unit data");
 	return data;
 }
--- jsonparser.h
+++ jsonparser.h
@@ -8,8 +8,8 @@ class Parser
 
 public:
     static std::map <std::string, std::string> jsonParser(std::istream& file);
-    static std::map <std::string, std::string> jsonParser(const std::string& file);
-
+    static std::map <std::string, std::string> jsonParser(const std::string& fname);
+    static std::string jsonParserFILE(const std::string& filename);
 };
 
 #endif
