0446799285842245c396e698a764fe79311dc776
bartaliszoltan <zoltanbartalis98@gmail.com>
2020-10-21 03:22:53 +0200

    fix attackspeed

--- character.cpp
+++ character.cpp
@@ -1,5 +1,6 @@
 #include "character.h"
 #include <fstream>
+#include <cmath>
 
 
 Character::Character(const std::string& name, double health, double damage, const double attackspeed) : name(name), health(health), damage(damage), attackspeed(attackspeed) {}
@@ -37,7 +38,7 @@ Character Character::parseUnit(const std::string& fname) {
 	while (getline(file, line)) {
 		for (unsigned int i = 0; i < line.size(); i++) {
 			if (line[i] == '"') { inside = 1 - inside; i++; }
-			if (inside or isdigit(line[i])) {
+			if (inside or isdigit(line[i]) or line[i]=='.') {
 
 				if (type == "name") name += line[i];
 				else if (type == "hp") hp += line[i];
@@ -56,15 +57,16 @@ Character Character::parseUnit(const std::string& fname) {
 
 
 void Character::attack(Character& defender) {
-	if (this->getAttackspeed() == 0) { defender.performAttack(*this); this->performAttack(defender); };
+	if (this->getAttackspeed() == 0) { this->performAttack(defender); defender.performAttack(*this); };
 	double A_Timer = 0, B_Timer = 0;
-	while (this->getHealth() > 0 and defender.getHealth() > 0) {
+	while (defender.getHealth() > 0 and this->getHealth() > 0) {
 
-
-		if (A_Timer > B_Timer) { this->performAttack(defender); }
-		else defender.performAttack(*this);
-		(A_Timer <= B_Timer) ?
-			(B_Timer -= A_Timer, A_Timer = this->getAttackspeed()) :
-			(A_Timer -= B_Timer, B_Timer = defender.getAttackspeed());
+		if (A_Timer > B_Timer) defender.performAttack(*this);
+		else this->performAttack(defender);
+		(A_Timer > B_Timer) ?
+			(A_Timer -= B_Timer, B_Timer = defender.getAttackspeed(), A_Timer = round(A_Timer*1000000)/1000000):
+			(B_Timer -= A_Timer, A_Timer = this->getAttackspeed(), B_Timer = round(B_Timer*1000000)/1000000);
 	}
 };
+
+
